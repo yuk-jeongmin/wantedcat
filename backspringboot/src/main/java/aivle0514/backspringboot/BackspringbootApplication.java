0a7d8a0cc5a6a2ed0cbf8b5613622afcc09ee37d
sjlee-catthe <ee395000@gmail.com>
2025-08-03 00:38:12 +0900

    start back

---
+++ backspringboot/src/main/java/aivle0514/backspringboot/BackspringbootApplication.java
@@ -0,0 +1,13 @@
+package aivle0514.backspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackspringbootApplication.class, args);
+	}
+
+}
