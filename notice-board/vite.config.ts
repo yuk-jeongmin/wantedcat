d1766bd81eb6c5b594adea6cfd386ce4e3ce9417
sjlee-catthe Lee <ee395000@gmail.com>
2025-08-05 15:35:16 +0000

    index.css 비우니까 그나마 유지됨

--- notice-board/vite.config.ts
+++ notice-board/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from 'path'
 
 const isGitpod = !!process.env.GITPOD_WORKSPACE_URL;
 const gitpodHost = process.env.GITPOD_WORKSPACE_URL
@@ -30,5 +31,10 @@ export default defineConfig({
               port: 5173,
             },
           }),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),  // ← 꼭 설정해야 '@/...' 경로가 동작합니다
+    },
   },
 });
