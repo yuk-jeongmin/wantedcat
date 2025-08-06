a335f100114f13e66805287e8a50f21073477a63
sjlee-catthe Lee <ee395000@gmail.com>
2025-08-06 05:03:08 +0000

    tailwind v3를 쓰자. v4는 용례가 부족해서 따라하기 힘들다

--- frontend2/vite.config.ts
+++ frontend2/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [react()],
@@ -15,4 +16,9 @@ export default defineConfig({
     host: true, // Gitpod 환경에서 서버 접속을 위해 필요
     allowedHosts: ['5173-sjleecatthe-wantedcat-7dxfzhg0f8g.ws-us120.gitpod.io']
   },
+  css:{
+    postcss:{
+      plugins: [tailwindcss()],
+    }
+  }
 });
